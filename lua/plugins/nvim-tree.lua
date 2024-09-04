6714435200be29b0e699ecd96914b3d077383028
Trevor <trevorhauter99@gmail.com>
2024-09-03 21:43:34 -0400

    Start lazyvim config with theme and nvim-tree

---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,17 @@
+return {
+  "nvim-tree/nvim-tree.lua",
+  keys = {
+    -- Define keybindings for nvim-tree
+    {'<C-n>', ':NvimTreeToggle<CR>', { noremap = true, silent = true }},
+    {'<leader>n', ':NvimTreeFindFile<CR>', { noremap = true, silent = true }},
+    {'<C-t>', ':NvimTreeCollapse<CR>', { noremap = true, silent = true }},
+  },
+  version = "*",
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+	config = function()
+    require("nvim-tree").setup {}
+  end,
+}
