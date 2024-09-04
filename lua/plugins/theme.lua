0bcccb54aa06f212572e5f0a41c0cdb2d19e0756
Trevor <trevorhauter99@gmail.com>
2024-09-04 19:50:09 -0400

    Replace theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,12 +1,24 @@
 return {
-  -- the colorscheme should be available when starting Neovim
   {
-    "folke/tokyonight.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
+    'sainnhe/gruvbox-material',
+    lazy = false,
+    priority = 1000,
     config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme tokyonight]])
-    end,
-  },
+      -- Optionally configure and load the colorscheme
+      -- directly inside the plugin declaration.
+      if vim.fn.has('termguicolors') == 1 then
+        vim.opt.termguicolors = true
+      end
+
+      vim.g.gruvbox_material_enable_italic = true
+      vim.g.gruvbox_material_enable_bold = true
+      
+      vim.g.gruvbox_material_transparent_background = 1
+      vim.o.background='dark'
+
+      vim.g.gruvbox_material_background = 'soft'
+      vim.g.gruvbox_material_better_performance = 1
+      vim.cmd.colorscheme('gruvbox-material')
+    end
+  }
 }
