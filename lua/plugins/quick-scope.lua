7670bc0857f4698bbc55176eb3750c00e5fca5c5
Trevor <trevorhauter99@gmail.com>
2024-09-05 20:47:17 -0400

    Add quickscope to config

---
+++ lua/plugins/quick-scope.lua
@@ -0,0 +1,15 @@
+vim.g.qs_highlight_on_keys = {'f', 'F', 't', 'T'}
+
+-- Define QuickScope highlights
+-- This updates our highlight colors to be in line with our color scheme automatically!
+vim.cmd([[
+  augroup qs_colors
+  autocmd!
+  autocmd ColorScheme * highlight QuickScopePrimary guifg='#afff5f' gui=underline ctermfg=155 cterm=underline
+  autocmd ColorScheme * highlight QuickScopeSecondary guifg='#5fffff' gui=underline ctermfg=81 cterm=underline
+  augroup END
+]])
+
+return {
+  "unblevable/quick-scope",
+}
