6c97d2f8686b8650399e9a07f14b1da30b112db0
Trevor <trevor@utilityapi.com>
2024-09-10 16:45:48 -0400

    Configure lua_ls to understand vim as a global variable

--- lua/config/lsp-zero.lua
+++ lua/config/lsp-zero.lua
@@ -95,6 +95,19 @@ require('mason-lspconfig').setup({
       })
     end,
 
+    lua_ls = function()
+      lspconfig.lua_ls.setup ({
+        settings = {
+          Lua = {
+            diagnostics = {
+              -- Get the language server to recognize the `vim` global
+              globals = {'vim'},
+            },
+          },
+        },
+      })
+    end,
+
     --pyright
     pyright = function()
       lspconfig.pyright.setup({
