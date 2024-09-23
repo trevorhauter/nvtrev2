7b6d66e0098678af63189b96f0d6f12e8ee961c3
Trevor Hauter <74263491+trevorhauter@users.noreply.github.com>
2024-09-23 09:26:46 -0400

    Add gitportal to config (#20)
    * Add gitportal to config
    
    * Rename cli, add method to open url automatically
    
    * Add keybindings
    
    * Update dev config
    
    * Add github path to plugin

---
+++ lua/plugins/gitportal.lua
@@ -0,0 +1,12 @@
+return {
+  'trevorhauter/gitportal.nvim',
+  config = function()
+    local gitportal = require("gitportal.core")
+    vim.keymap.set("v", "<leader>gp", function() gitportal.open_file_in_browser() end)
+    vim.keymap.set("n", "<leader>gp", function() gitportal.open_file_in_browser() end)
+    vim.keymap.set('n', '<leader>ig', function()
+      gitportal.open_file_in_neovim(vim.fn.input("Github link > "));
+    end)
+
+  end,
+}
