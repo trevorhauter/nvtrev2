57a328eb7f0d11a14b1c87ac8275b0434cc99cf8
Trevor <trevor@utilityapi.com>
2024-09-24 10:17:41 -0400

    Update gitportal config'

--- lua/plugins/gitportal.lua
+++ lua/plugins/gitportal.lua
@@ -1,12 +1,9 @@
 return {
   'trevorhauter/gitportal.nvim',
   config = function()
-    local gitportal = require("gitportal.core")
+    local gitportal = require("gitportal")
     vim.keymap.set("v", "<leader>gp", function() gitportal.open_file_in_browser() end)
     vim.keymap.set("n", "<leader>gp", function() gitportal.open_file_in_browser() end)
-    vim.keymap.set('n', '<leader>ig', function()
-      gitportal.open_file_in_neovim(vim.fn.input("Github link > "));
-    end)
-
+    vim.keymap.set('n', '<leader>ig', function() gitportal.open_file_in_neovim() end)
   end,
 }
