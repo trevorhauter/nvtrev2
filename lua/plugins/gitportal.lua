884599c6008078d1c8811f5bd8f4d0510c11530e
Trevor <trevorhauter99@gmail.com>
2024-09-28 22:17:33 -0400

    Update test config

--- lua/plugins/gitportal.lua
+++ lua/plugins/gitportal.lua
@@ -2,6 +2,11 @@ return {
   dir='/Users/trevorhauter/Code/gitportal.nvim',
   config = function()
     local gitportal = require("gitportal")
+
+    gitportal.setup({
+      always_include_current_line = true,
+    })
+
     vim.keymap.set("v", "<leader>gp", function() gitportal.open_file_in_browser() end)
     vim.keymap.set("n", "<leader>gp", function() gitportal.open_file_in_browser() end)
     vim.keymap.set('n', '<leader>ig', function() gitportal.open_file_in_neovim() end)
