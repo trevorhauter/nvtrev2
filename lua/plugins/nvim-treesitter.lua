dce25a5f2f717a3ee1d4bdbbc370bbcbbe8e6995
Trevor <trevorhauter99@gmail.com>
2024-10-01 18:53:29 -0400

    Add vim-matchup

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,32 +1,38 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    config = function () 
+    config = function ()
       local configs = require("nvim-treesitter.configs")
 
       configs.setup({
-          ensure_installed = { 
-            "bash", 
-            "c", 
-            "c_sharp", 
-            "cmake", 
-            "css", 
+          ensure_installed = {
+            "bash",
+            "c",
+            "c_sharp",
+            "cmake",
+            "css",
             "gitcommit",
-            "gitignore", 
-            "html", 
-            "htmldjango", 
+            "gitignore",
+            "html",
+            "htmldjango",
             "java",
-            "javascript", 
+            "javascript",
             "json",
-            "lua", 
-            "python", 
-            "query", 
-            "vim", 
-            "vimdoc", 
+            "lua",
+            "python",
+            "query",
+            "vim",
+            "vimdoc",
           },
           sync_install = false,
           highlight = { enable = true },
-          indent = { enable = true },  
+          indent = { enable = true },
+
+          matchup = {
+            enable = true,
+          },
+
         })
-    end
+
+        end
  }
