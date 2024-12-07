1fc8db8b818a9de707b5f8a82299d0a441ffe56d
Trevor <trevorhauter99@gmail.com>
2024-12-06 22:25:35 -0500

    Add context into nvtrev2 nvtrev2/issues#18

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -5,7 +5,6 @@ return {
         { "<C-n>", ":NvimTreeToggle<CR>", { noremap = true, silent = true } },
         { "<leader>n", ":NvimTreeFindFile<CR>", { noremap = true, silent = true } },
         { "<C-t>", ":NvimTreeCollapse<CR>", { noremap = true, silent = true } },
-        { "<C-a>", "<cmd>lua toggle_width_adaptive()<CR>", { noremap = true, silent = true } },
     },
     version = "*",
     lazy = false,
