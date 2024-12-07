f34154739e99512265d1af49f17a1d4461b764fb
Trevor <trevorhauter99@gmail.com>
2024-12-06 22:03:07 -0500

    Fixed my god awful weird solution for nvim-tree responsive width

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,23 +1,3 @@
--- The following code is to allow for us to toggle the width of nvim-tree in cases
--- that we have long file names/paths :)
-_G.VIEW_WIDTH_FIXED = 30
-_G.view_width_max = _G.VIEW_WIDTH_FIXED -- fixed to start
-
--- toggle the width and redraw
-_G.toggle_width_adaptive = function()
-    if _G.view_width_max == -1 then
-        _G.view_width_max = _G.VIEW_WIDTH_FIXED
-    else
-        _G.view_width_max = -1
-    end
-
-    require("nvim-tree.api").tree.reload()
-end
-
-_G.get_view_width_max = function()
-    return _G.view_width_max
-end
-
 return {
     "nvim-tree/nvim-tree.lua",
     keys = {
@@ -34,12 +14,7 @@ return {
     },
     config = function()
         require("nvim-tree").setup({
-            view = {
-                width = {
-                    min = 30,
-                    max = _G.get_view_width_max,
-                },
-            },
+            view = { adaptive_size = true },
             filters = { custom = { "^.git$", "^.gitignore$" } },
         })
     end,
