feda94ddb4ee70cd33f5c2ccb12bdd823031e73f
Trevor <trevorhauter99@gmail.com>
2024-12-06 23:08:55 -0500

    Remove weird extra line number info for folds

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -61,7 +61,7 @@ vim.opt.fillchars:append({ fold = " " })
 vim.o.foldnestmax = 3
 vim.o.foldminlines = 1
 vim.o.foldenable = false
-vim.o.foldcolumn = "1" -- '0' is not bad
+vim.o.foldcolumn = "0" -- '0' is not bad
 vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 
