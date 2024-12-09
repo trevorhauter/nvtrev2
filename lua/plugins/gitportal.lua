ea36cff9c85fc690fb752cf22d3f7ac006316a31
Trevor <trevor@utilityapi.com>
2024-12-09 10:26:40 -0500

    Add copy to clipboard command for gitportal.nvim

--- lua/plugins/gitportal.lua
+++ lua/plugins/gitportal.lua
@@ -14,5 +14,13 @@ return {
         vim.keymap.set("n", "<leader>ig", function()
             gitportal.open_file_in_neovim()
         end)
+
+        vim.keymap.set("n", "<leader>gc", function()
+            gitportal.copy_link_to_clipboard()
+        end)
+
+        vim.keymap.set("v", "<leader>gc", function()
+            gitportal.copy_link_to_clipboard()
+        end)
     end,
 }
