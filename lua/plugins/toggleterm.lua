0b9ca3c5a0d2ccde76e4e7a994187ed85bb46dc2
Trevor <trevor@utilityapi.com>
2024-12-19 11:51:08 -0500

    Update border for toggleterm

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,5 +1,5 @@
 return {
     "akinsho/toggleterm.nvim",
     version = "*",
-    opts = { open_mapping = [[<c-g>]], direction = "float" },
+    opts = { open_mapping = [[<c-g>]], direction = "float", float_opts = { border = "curved" } },
 }
